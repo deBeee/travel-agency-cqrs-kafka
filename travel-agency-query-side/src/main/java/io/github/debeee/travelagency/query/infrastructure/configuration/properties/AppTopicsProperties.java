81fb1fafe4271b83e5ac0f681db85e0cc4257b74
deBeee <dominik.bytnar@gmail.com>
2026-09-15 16:11:14 +0200

    transform booking events into daily availability updates with kafka streams

--- travel-agency-query-side/src/main/java/io/github/debeee/travelagency/query/infrastructure/configuration/properties/AppTopicsProperties.java
+++ travel-agency-query-side/src/main/java/io/github/debeee/travelagency/query/infrastructure/configuration/properties/AppTopicsProperties.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.topics")
 public record AppTopicsProperties(
+        String bookings,
         String availability,
         String availabilityDlt
 ) {
---
+++ travel-agency-query-side/src/main/java/io/github/debeee/travelagency/query/infrastructure/streams/KafkaStreamsConfig.java
@@ -0,0 +1,17 @@
+package io.github.debeee.travelagency.query.infrastructure.streams;
+
+import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.config.StreamsBuilderFactoryBeanConfigurer;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamsConfig {
+    @Bean
+    public StreamsBuilderFactoryBeanConfigurer streamsBuilderFactoryBeanConfigurer() {
+        return factoryBean -> factoryBean.setStreamsUncaughtExceptionHandler(exception ->
+                StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD);
+    }
+}
